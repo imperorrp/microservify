6a9b5216fe826e0c6f88effea95d9d1132cf7268
Ratish Panda <ratishp2001@gmail.com>
2023-09-29 04:00:34 +0530

    Added README and base streamlit app

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# microservify
-AI powered microservice generator 
+# Microservify
+AI powered microservice code generator (brought to you by prompt chain magic)
+
+## About 
+
+Microservify is a simple Streamlit web app built around the core functionality of automating microservice code creation. Langchain is used for prompt templating and chaining, and Cohere's LLM models used as the generator. 
+
+As with existing LLM based tools, producing perfect code that does exactly what it needs to is hard for generative AI. The creativity and uniqueness of requirements is proportional to the amount of bugs and flawed logic for generated code- meaning humans are still needed to look over and edit the microservice code before pushing into service. The bulk initial work, though, can be automated to a fairly good degree.
+
+What makes the microservices architecture kind of code components especially suited for AI generation? Typically small code sizes falling well within token limits for most LLM models, lack of requirement to understand the entirety of a more monolithic codebase sprawl, and a user can just include relevant information in the initial prompt. 
+
+Microservify leverages these benefits, making use of Langchain for prompt chaining to get step by step logic- first planning, then generating, and finally reviewing to pre-empt the human bug fixing process after generation. 
+
+## Setup and Usage
+
+1. Clone this repository 
+
+`git clone https://github.com/imperorrp/microservify.git`
+
+2. Install dependencies
+
+`pip install -r requirements.txt`
+
+3. Run with
+
+`streamlit run streamlit_app.py` 
+
+A local instance should then be up on localhost
+## Coming Soon 
+
+Support for more LLM providers and models.
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,38 @@
-#placeholder comment
+#streamlit entry point
+
+#importing dependencies
+import streamlit as st
+from langchain.llms import Cohere
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+#setting main page title
+st.title("🦜🔗 Microservify - Microservice Code Generator ")
+
+#provide llm provider options (OpenAI, Anthropic, Cohere, etc.)
+#currently just a Cohere model
+cohere_api_key = st.sidebar.text_input("Cohere API Key", type="password")
+
+#first step of logic, generating microservice outline from user prompt
+def microservice_outline(concept):
+    #instantiate LLM model
+    llm = Cohere(model_name="command-nightly", cohere_api_key=cohere_api_key)
+    #prompt
+    template = "You are an expert programmer well versed with multiple programming languages and coding paradigms. You are very proficient at planning and creating microservices with code. You have been granted the task of generating a comprehensive outline for a microservice, which will include all the flows and top level architectural design describing it. The required microservice is described as follows: {concept}."
+    prompt = PromptTemplate(input_variables=["topic"], template=template)
+    prompt_query = prompt.format(concept=concept)
+    #run llm model
+    response = llm(prompt_query)
+    #print results
+    return st.info(response)
+
+#todo: rest of the logic chain (generation + review)
+
+with st.form("form1"):
+    topic_text = st.text_input("Describe the concept/idea of the microservice you want generated:", "")
+    submitted = st.form_submit_button("Submit")
+    if not cohere_api_key:
+        st.info("Please enter your Cohere API key to continue.")
+    elif submitted:
+        microservice_outline(topic_text)
+
